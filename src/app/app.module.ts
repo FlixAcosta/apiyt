d1e9f417768e34f24a16c9137a6920f3c6e5d209
FlixAcosta <stop.pfr@gmail.com>
2017-12-26 23:31:04 -0600

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    YoutubeVideoPlayer,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { DomSanitizer } from '@angular/platform-browser';
+//import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import 'rxjs/add/operator/map';
+
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_TOKEN = 'AIzaSyAJk1xUI72YYfBMgEc84gjHUX-k2AN6-B0';
+
+const yt = "http://www.youtube.com/embed/"
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+posts : any;
+videos : any;
+search = this.search;
+
+  constructor(public navCtrl: NavController, public http: Http, private sanitizer: DomSanitizer) {
+
+
+}
+	postVideo() {
+
+	this.http.get(`${BASE_URL}?q=${this.search}&part=snippet&key=${API_TOKEN}`).map(res => res.json()).subscribe(data => {
+        this.posts = this.sanitizer.bypassSecurityTrustResourceUrl(`${yt}${data.items[1].id.videoId}`);
+        console.log(this.posts);
+    });
+
+  this.http.get(`${BASE_URL}?q=${this.search}&part=snippet&key=${API_TOKEN}`).map(res => res.json()).subscribe(data => {
+        this.videos = this.sanitizer.bypassSecurityTrustResourceUrl(`${yt}${data.items[2].id.videoId}`);
+        console.log(this.videos);
+    });
+	
+      		
 
   }
 
